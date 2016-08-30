14536a07e578ec832d76fba914a32d02443a848c
bkawan <bikeshkawang@gmail.com>
2016-08-30 13:27:13 +0545

    minor updates on git ignore add

--- jobcrawl/spiders/alljobs.py
+++ jobcrawl/spiders/alljobs.py
@@ -35,7 +35,7 @@ class AllJobsSpider(scrapy.Spider):
         except:
             total_pages = 2000
 
-        for i in range(1):
+        for i in range(total_pages):
             page_link = "http://www.alljobs.co.il/SearchResultsGuest.aspx?page=%s&position=&type=&freetxt=&city=&region=" % str(i + 1)
             yield scrapy.Request(
                 page_link, self.parse_each_page,
