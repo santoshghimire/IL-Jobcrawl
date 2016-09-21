d9e3f455cbf2571088ddd953e8e3f3589df7f735
Santosh Ghimire <santosh.ghimire33@gmail.com>
2016-09-21 08:29:55 +0545

    changes for formatting

--- jobcrawl/spiders/drushim.py
+++ jobcrawl/spiders/drushim.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.shell import inspect_response
 from jobcrawl.items import JobItem
 
 import sys
@@ -14,28 +13,33 @@ class DrushimSpider(scrapy.Spider):
     allowed_domains = ["drushim.co.il"]
     start_urls = (
         'https://www.drushim.co.il/jobs/search',
-
     )
 
     def __init__(self):
 
-        sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
+        sys.stdout = codecs.getwriter(
+            locale.getpreferredencoding())(sys.stdout)
         reload(sys)
         sys.setdefaultencoding('utf-8')
 
     def parse(self, response):
         # inspect_response(response,self)
+        main_content_job_list = response.xpath(
+            "//div[@id='MainContent_JobList_jobList']")
+        job_container = main_content_job_list.xpath(
+            ".//div[@class='jobContainer']")
 
-        main_content_job_list = response.xpath("//div[@id='MainContent_JobList_jobList']")
-        job_container = main_content_job_list.xpath(".//div[@class='jobContainer']")
-
-        job_link_list = job_container.xpath(".//a[@class='fullPage']/@href").extract()
+        job_link_list = job_container.xpath(
+            ".//a[@class='fullPage']/@href").extract()
         # job_link_list = job_link_list[1:2]
 
         for job_link in job_link_list:
-            yield scrapy.Request(job_link, callback=self.parse_each_job, dont_filter=True)
+            yield scrapy.Request(
+                job_link, callback=self.parse_each_job,
+                dont_filter=True)
 
-        next_pagi = main_content_job_list.xpath(".//a[@class='pager lightBg stdButton']/@href").extract_first()
+        next_pagi = main_content_job_list.xpath(
+            ".//a[@class='pager lightBg stdButton']/@href").extract_first()
         # next_pagi = 'https://www.drushim.co.il/jobs/?page=2'
 
         if next_pagi:
@@ -43,15 +47,8 @@ class DrushimSpider(scrapy.Spider):
 
     def parse_each_job(self, response):
         # inspect_response(response,self)
-        if response.status != 200:
-            self.logger.error("{}\n ERROR Code {}: {} \n {}".format("*"*30, response.status, response.url, "*"*30))
-        else:
-            self.logger.info("{}\n  Status Code {} OK: {} \n {}".format("*"*30, response.status, response.url, "*"*30))
-
-
         job_container = response.xpath("//div[@class='jobContainer']")
         job_fields_sel_list = response.xpath("//div[@class='jobFields']/*")
-
 
         try:
             job_id = response.url.split("/")[-3]
@@ -59,54 +56,66 @@ class DrushimSpider(scrapy.Spider):
             job_id = ""
 
         try:
-            job_title = job_container.xpath(".//h1[@class='jobName']/text()").extract_first()
+            job_title = job_container.xpath(
+                ".//h1[@class='jobName']/text()").extract_first()
         except:
             job_title = ""
 
-
         try:
-            company = job_container.xpath(".//div[@class='fieldContainer vertical first']/"
-                                          "span[@class='fieldTitle']/text()").extract_first()
+            company = job_container.xpath(
+                ".//div[@class='fieldContainer vertical first']/"
+                "span[@class='fieldTitle']/text()").extract_first()
 
         except:
             company = ""
 
         try:
-            company_jobs = job_container.xpath(".//a[@class='companyLink noToggle']/@href").extract_first()
+            company_jobs = job_container.xpath(
+                ".//a[@class='companyLink noToggle']/@href").extract_first()
         except:
             company_jobs = ""
 
-        field_container_sel_list = job_container.xpath(".//div[@class='fieldContainer horizontal']")
+        field_container_sel_list = job_container.xpath(
+            ".//div[@class='fieldContainer horizontal']")
         try:
-            job_description = "\n".join(job_fields_sel_list.xpath("string()").extract())
+            job_description = "\n".join(
+                job_fields_sel_list.xpath("string()").extract())
             job_post_date = re.findall(r'\d\d/\d\d/\d\d\d\d', job_description)
 
             if job_post_date:
                 job_post_date = job_post_date[0]
             else:
 
-                field_container_sel_list = job_container.xpath(".//div[@class='fieldContainer horizontal']")
-                job_post_date = field_container_sel_list[-1].xpath(".//span[@class='fieldText rtl']/text()").extract_first()
+                field_container_sel_list = job_container.xpath(
+                    ".//div[@class='fieldContainer horizontal']")
+                job_post_date = field_container_sel_list[-1].xpath(
+                    ".//span[@class='fieldText rtl']/text()").extract_first()
 
         except:
 
             job_post_date = ""
 
-
         try:
-            country_areas = job_fields_sel_list[4].xpath(".//span/text()")[-1].extract()
+            country_areas = job_fields_sel_list[4].xpath(
+                ".//span/text()")[-1].extract()
         except:
             country_areas = ""
 
         try:
-            category = field_container_sel_list[2].xpath(".//a/text()").extract_first()
+            category = field_container_sel_list[2].xpath(
+                ".//a/text()").extract_first()
         except:
             category = ""
 
         try:
-            job_description = "\n".join(job_fields_sel_list[1:].xpath("string()").extract())
-            job_description = job_description.replace(re.findall(r'\d\d/\d\d/\d\d\d\d\n?(.*)', job_description)[0], "")
-
+            job_description = "\n".join(
+                job_fields_sel_list[1:].xpath("string()").extract())
+            job_description = job_description.replace(
+                re.findall(
+                    r'\d\d/\d\d/\d\d\d\d\n?(.*)',
+                    job_description
+                )[0], ""
+            )
 
         except:
             job_description = ""
@@ -127,5 +136,4 @@ class DrushimSpider(scrapy.Spider):
             'AllJobs_Job_class': '',
             'unique_id': 'drushim_{}'.format(job_id)
         }
-
         yield item
