22136704ccbc3ab0b201a565ae812f404657ed2b
Santosh Ghimire <santosh.ghimire33@gmail.com>
2021-05-12 11:59:30 +0545

    Update drushim

--- jobcrawl/spiders/drushim.py
+++ jobcrawl/spiders/drushim.py
@@ -14,7 +14,7 @@ class DrushimSpider(scrapy.Spider):
     name = "drushim"
     allowed_domains = ["drushim.co.il"]
     start_urls = (
-        'https://www.drushim.co.il/jobs/search',
+        'https://www.drushim.co.il/jobs/search/%22%22/?ssaen=1',
     )
 
     def __init__(self):
@@ -26,113 +26,88 @@ class DrushimSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        main_content_job_list = response.xpath(
-            "//div[@id='MainContent_JobList_jobList']")
-        job_container_list = main_content_job_list.xpath(
-            ".//div[@class='jobContainer']")
-
+        job_container_list = response.xpath(
+            "//div[@class='job-item-main pb-3 job-hdr']")
         for job_container in job_container_list:
 
             job_link = job_container.xpath(
-                ".//a[@class='fullPage']/@href").extract_first()
+                ".//div[@class='flex nowrap align-self-center pc-view open-job text-center']/a/@href").extract_first()
 
             try:
-                job_id = job_link.split("/")[-3]
+                job_id = "-".join(job_link.split("/")[-2:])
             except:
                 job_id = ""
 
             try:
                 job_title = job_container.xpath(
-                    ".//h2[@class='jobName']/text()").extract_first()
+                    ".//span[@class='job-url primary--text font-weight-bold primary--text']").xpath(
+                        "normalize-space(string())").extract_first()
             except:
                 job_title = ""
 
             try:
                 company = job_container.xpath(
-                    ".//div[@class='fieldContainer vertical first']/"
-                    "span[@class='fieldTitle']/text()").extract_first()
-
+                    ".//div[@class='layout job-details-top mt-md-2 align-baseline']"
+                    "/div[@class='flex grow-none ml-3']/p").xpath("normalize-space(string())").extract_first()
             except:
                 company = ""
 
             try:
                 company_jobs = job_container.xpath(
-                    ".//a[@class='companyLink noToggle']/@href"
-                ).extract_first()
+                    ".//div[@class='layout job-details-top mt-md-2 align-baseline']"
+                    "/div[@class='flex grow-none ml-3']/p/a/@href").extract_first()
             except:
                 company_jobs = job_link
 
-            job_fields_sel_list = job_container.xpath(
-                ".//div[@class='jobFields']/*")
-            job_fields_sel_list = job_fields_sel_list[1:]
+            job_description = ""
+
+            job_sub_details = job_container.xpath(".//div[@class='layout job-details-sub']").xpath(
+                "normalize-space(string())").extract_first()
+            jsd_val = []
+            if job_sub_details:
+                jsd_val = job_sub_details.split("|")
+
+            country_areas = jsd_val[0].strip() if jsd_val else ""
+            category = jsd_val[2].strip() if jsd_val and len(jsd_val) == 4 else ""
+            job_post_date = jsd_val[-1].strip() if jsd_val and len(jsd_val) == 4 else ""
 
             try:
-                job_description = "\n".join(
-                    job_fields_sel_list.xpath("string()").extract()
-                )
-            except:
-                job_description = ""
+                job_post_date_num = re.findall(r'[\d]+', job_post_date)[0]
+                job_post_date_num = int(job_post_date_num)
 
-            try:
-                country_areas = job_fields_sel_list[3].xpath(
-                    ".//span/text()")[-1].extract()
-            except:
-                country_areas = ""
+                if job_post_date_num:
 
-            try:
-                category = job_fields_sel_list[4].xpath(
-                    ".//span/text()")[-1].extract()
-            except:
-                category = ""
+                    second = 'שְׁנִיָה'.decode('utf-8')
+                    seconds = 'שניות'.decode('utf-8')
+                    minute = 'דַקָה'.decode('utf-8')
+                    minutes = 'דקות'.decode('utf-8')
+                    hour = 'שָׁעָה'.decode('utf-8')
+                    hours = 'שעות'.decode('utf-8')
+                    day = 'יְוֹם'.decode('utf-8')
+                    days = 'ימים'.decode('utf-8')
+                    # month = 'חוֹדֶשׁ'.decode('utf-8')
+                    # months = 'חודשים'.decode('utf-8')
+                    hms = [second, seconds, minute, minutes, hour, hours]
 
-            try:
-                job_post_date = job_container.xpath(
-                    ".//span[@class='jobDate rtl']/text()"
-                ).extract_first()
+                    if day in job_post_date:
+                        job_post_date = datetime.date.today() - \
+                            datetime.timedelta(days=job_post_date_num)
+                        job_post_date = job_post_date.strftime("%d/%m/%Y")
+                    elif days in job_post_date:
+                        job_post_date = datetime.date.today() - \
+                            datetime.timedelta(days=job_post_date_num)
+                        job_post_date = job_post_date.strftime("%d/%m/%Y")
 
-                try:
-                    job_post_date_num = re.findall(r'[\d]+', job_post_date)[0]
-                    job_post_date_num = int(job_post_date_num)
+                    elif [x for x in hms if x in job_post_date]:
+                        job_post_date = datetime.date.today()
+                        job_post_date = job_post_date.strftime("%d/%m/%Y")
 
-                    if job_post_date_num:
+                    elif job_post_date_num == 0:
+                        job_post_date = datetime.date.today()
+                        job_post_date = job_post_date.strftime("%d/%m/%Y")
 
-                        second = 'שְׁנִיָה'.decode('utf-8')
-                        seconds = 'שניות'.decode('utf-8')
-                        minute = 'דַקָה'.decode('utf-8')
-                        minutes = 'דקות'.decode('utf-8')
-                        hour = 'שָׁעָה'.decode('utf-8')
-                        hours = 'שעות'.decode('utf-8')
-                        day = 'יְוֹם'.decode('utf-8')
-                        days = 'ימים'.decode('utf-8')
-                        # month = 'חוֹדֶשׁ'.decode('utf-8')
-                        # months = 'חודשים'.decode('utf-8')
-                        hms = [second, seconds, minute, minutes, hour, hours]
-
-                        if day in job_post_date:
-                            job_post_date = datetime.date.today() - \
-                                datetime.timedelta(days=job_post_date_num)
-                            job_post_date = job_post_date.strftime("%d/%m/%Y")
-                        elif days in job_post_date:
-                            job_post_date = datetime.date.today() - \
-                                datetime.timedelta(days=job_post_date_num)
-                            job_post_date = job_post_date.strftime("%d/%m/%Y")
-
-                        elif [x for x in hms if x in job_post_date]:
-                            job_post_date = datetime.date.today()
-                            job_post_date = job_post_date.strftime("%d/%m/%Y")
-
-                        elif job_post_date_num == 0:
-                            job_post_date = datetime.date.today()
-                            job_post_date = job_post_date.strftime("%d/%m/%Y")
-
-                        else:
-                            job_post_date = job_post_date
-
-                except:
-                    job_post_date = job_container.xpath(
-                        ".//span[@class='jobDate rtl']/text()"
-                    ).extract_first()
-
+                    else:
+                        job_post_date = job_post_date
             except:
                 job_post_date = ""
 
@@ -153,11 +128,3 @@ class DrushimSpider(scrapy.Spider):
             }
 
             yield item
-
-        next_pagi = main_content_job_list.xpath(
-            ".//a[@class='pager lightBg stdButton']/@href"
-        ).extract_first()
-        # next_pagi = 'https://www.drushim.co.il/jobs/?page=2'
-
-        if next_pagi:
-            yield scrapy.Request(next_pagi, callback=self.parse)
