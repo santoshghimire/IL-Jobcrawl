98131629d941fb7b6d5c0aeb26cd4ba185e44400
Santosh Ghimire <santosh.ghimire33@gmail.com>
2023-08-14 11:45:19 +0545

    Fix endtime

--- jobcrawl/endtime_check.py
+++ jobcrawl/endtime_check.py
@@ -1,6 +1,6 @@
 import datetime
 
-def reached_endtime(self):
+def reached_endtime():
     now = datetime.datetime.utcnow()
     endtime = now.replace(hour=12, minute=30, second=0, microsecond=0)
     return now > endtime
